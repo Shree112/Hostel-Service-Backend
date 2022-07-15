c317262ff736107a1a7fabb03abcbd8b49819587
Shree112 <ahuja.shree31@gmail.com>
2022-07-15 23:59:54 +0530

    changed port

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
 });
 
-app.listen(4000, function () {
-  console.log("Server is running on port: " + 4000);
+app.listen(process.env.PORT || 4000, function () {
+  console.log("Server is running on port: " + process.env.PORT || 4000);
 });
